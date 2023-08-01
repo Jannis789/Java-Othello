6cd4f46f382fba9fe28ac557a1481457e96ed021
S1rPh0en1x <jannis.rustige@gmail.com>
2023-08-01 16:50:27 +0200

    Fix GameAction

--- GameAction.java
+++ GameAction.java
@@ -26,283 +26,256 @@ import java.util.Arrays;
 Die Vektoren<-1,0> und < 1,0> zeigen in vertikale Richtung (oben
  */
 
-
 public class GameAction {
 
-	private static GameBoard gameBoard;
+    private static GameBoard gameBoard;
 
-	private static JButton clickedButton;
+    private static JButton clickedButton;
 
-	public static void gameAction() {
-		gameBoard = Main.getGameBoard();
-		ActionListener actionListener = new ActionListener() {
-			@Override
+    public static void gameAction() {
+        gameBoard = Main.getGameBoard();
+        ActionListener actionListener = new ActionListener() {
+            @Override
 
-			public void actionPerformed(ActionEvent e) {
-				clickedButton = (JButton) e.getSource();
-				int[] selectedTile = getPositions();
+            public void actionPerformed(ActionEvent e) {
+                clickedButton = (JButton) e.getSource();
+                int[] selectedTile = getPositions();
                 int player;
-				if (Main.currentTurn % 2 != 0) {
-					player = 0;
-				} else {
-					player = 1;
-				}
-                if (isTileBlank(selectedTile)) {
-                    List < int[] > directionVectors = getDirectionVectors(selectedTile, player);
-                    List < int[][] > d_intArrayList = combinePositionAndDirections(selectedTile, directionVectors);
-                    // read2DArray(d_intArrayList);
-                    checkNextTiles(d_intArrayList, player);
+                if (Main.currentTurn % 2 != 0) {
+                    player = 0;
+                } else {
+                    player = 1;
                 }
-			}
-		};
-
-		for (int i = 0; i < gameBoard.getTilesPerRow(); i++) {
-			for (int j = 0; j < gameBoard.getTilesPerRow(); j++) {
-				gameBoard.getButton(i, j).addActionListener(actionListener);
-			}
-		}
-        
-	}
-    
-	public static int[] getPositions() {
-		int[] positions = new int[2];
-
-		for (int i = 0; i < gameBoard.getTilesPerRow(); i++) {
-			for (int j = 0; j < gameBoard.getTilesPerRow(); j++) {
-				if (gameBoard.getButton(i, j) == clickedButton) {
-					positions[0] = i;
-					positions[1] = j;
-					// System.out.println("Button wurde gedrückt --> x:" + i + "|y:" + j);
-					return positions;
-				}
-			}
-		}
-
-		return positions;
-	}
-
-	public static void colorize(List < int[] > positionList, Color color) {
-		for (int[] tilePos: positionList) {
-			// System.out.println("Kästschen wurde Eingefärbt --> x:" + tilePos[0] + "|y:" + tilePos[1]);
-			gameBoard.getButton(tilePos[0], tilePos[1]).setBackground(color);;
-		}
-	}
-
-     public static boolean isTileBlank(int[] currentPosition) {
-		// player = 0 (Schwarz) 
-		// player = 1 (Weiß)
-		int x = currentPosition[0];
-		int y = currentPosition[1];
-
-		// Prüft, ob das aktuelle Kästchen leer ist (weder Schwarz noch Weiß).
-		boolean blank = true;
-		// System.out.println("Position erhalten --> x:" + x + "|y:" + y);
-
-		for (int[] tilePos: Main.blackTilesPositionList) {
-			if (tilePos[0] == x && tilePos[1] == y) {
-				blank = false;
-				break;
-			}
-		}
-
-		if (blank) {
-			for (int[] tilePos: Main.whiteTilesPositionList) {
-				if (tilePos[0] == x && tilePos[1] == y) {
-					blank = false;
-					break;
-				}
-			}
-		}
-		return blank;
-	}
-
-    private static List<int[]> getDirectionVectors(int[] currentPosition, int player) {
-        // Die Liste der Richtungsvektoren für die Überprüfung der benachbarten Kästchen.
-        int[][] vectorsList = { {-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {0, -1}, {0, 1}, {-1, 0}, {1, 0} };
-    
-        List<int[]> directionsList = new ArrayList<>(); // Richtungsvektoren zu den anliegenden Positionen
-    
-        List<int[]> enemyTilesPositionList;
-    
-        if (player == 0) {
-            enemyTilesPositionList = Main.whiteTilesPositionList;
-        } else {
-            enemyTilesPositionList = Main.blackTilesPositionList;
-        }
-    
-        int boardSize = gameBoard.getTilesPerRow(); // Die Anzahl der Kästchen pro Zeile auf dem Spielbrett
-    
-        // Durchlaufe alle Richtungsvektoren, um die benachbarten Kästchen zu überprüfen.
-        for (int[] vector : vectorsList) {
-            int newX = currentPosition[0] + vector[0];
-            int newY = currentPosition[1] + vector[1];
-    
-            // Überprüfe, ob die Position innerhalb der Grenzen des Spielbretts liegt
-            if (newX >= 0 && newX < boardSize && newY >= 0 && newY < boardSize) {
-                // Durchlaufe die Positionenliste des Gegners, um zu prüfen, ob das Kästchen in der Richtung besetzt ist.
-                for (int[] enemyTilePos : enemyTilesPositionList) {
-                    if (newX == enemyTilePos[0] && newY == enemyTilePos[1]) {
-                        directionsList.add(new int[]{vector[0], vector[1]});
-                        break;
-                    }
+                if (isTileBlank(selectedTile)) {
+                    addNewPositions(selectedTile, player);
                 }
             }
+        };
+
+        for (int i = 0; i < gameBoard.getTilesPerRow(); i++) {
+            for (int j = 0; j < gameBoard.getTilesPerRow(); j++) {
+                gameBoard.getButton(i, j).addActionListener(actionListener);
+            }
         }
-    
-        return directionsList;
+
     }
 
-	private static List < int[][] > combinePositionAndDirections(int[] positionArray, List < int[] > directionArray) {
-		List < int[][] > combinedList = new ArrayList<>();
+    public static int[] getPositions() {
+        int[] positions = new int[2];
 
-		for (int[] vector: directionArray) {
-			int[][] pd = {
-				{
-					positionArray[0], positionArray[1]
-				},
-				{
-					vector[0], vector[1]
-				}
-			};
-			combinedList.add(pd);
-		}
-
-		return combinedList;
-	}
-
-	public static boolean isEnemyTile(int[] enemyTile, int player) {
-		// player = 0 (Schwarz) 
-		// player = 1 (Weiß)
-		int x = enemyTile[0];
-		int y = enemyTile[1];
-		List < int[] > playersTilesPositionList = new ArrayList<>(); // Eigene eingefärbte Felder
-		List < int[] > enemysTilesPositionList = new ArrayList<>(); // Gegnerische eingefärbte Felder
-		if (player == 0) {
-			enemysTilesPositionList.addAll(Main.whiteTilesPositionList);
-			playersTilesPositionList.addAll(Main.blackTilesPositionList);
-		} else if (player == 1) {
-			enemysTilesPositionList.addAll(Main.blackTilesPositionList);
-			playersTilesPositionList.addAll(Main.whiteTilesPositionList);
-		}
-
-		for (int[] tile: enemysTilesPositionList) {
-			if (tile[0] == x && tile[1] == y) {
-				return true;
-			}
-
-		}
-
-		return false;
-	}
-
-	private static boolean isPlayerTile(int[] enemyTile, int player) {
-		// player = 0 (Schwarz) 
-		// player = 1 (Weiß)
-		int x = enemyTile[0];
-		int y = enemyTile[1];
-		List < int[] > playersTilesPositionList = new ArrayList<>(); // Eigene eingefärbte Felder
-		List < int[] > enemysTilesPositionList = new ArrayList<>(); // Gegnerische eingefärbte Felder
-		if (player == 0) {
-			enemysTilesPositionList.addAll(Main.whiteTilesPositionList);
-			playersTilesPositionList.addAll(Main.blackTilesPositionList);
-		} else if (player == 1) {
-			enemysTilesPositionList.addAll(Main.blackTilesPositionList);
-			playersTilesPositionList.addAll(Main.whiteTilesPositionList);
-		}
-
-		for (int[] tile: playersTilesPositionList) {
-			if (tile[0] == x && tile[1] == y) {
-				return true;
-			}
-
-		}
-
-		return false;
-	}
-    private static void checkNextTiles(List<int[][]> combinedList, int player) {
-        // Man guckt wie oft man den Vektor addieren muss, bis isPlayerTile() = true erreicht ist
-        List<int[]> validDirection = new ArrayList<>();
-        List<int[]> changedPositions = new ArrayList<>(); // Declare and initialize the changedPositions list
-        int[] position = new int[2];
-        boolean reachedPlayerTile = false;
-        changedPositions.clear();
-        boolean reachedEnemyTile = false;
-        for (int i = 0; i < combinedList.size(); i++) {
-            int[][] entry = combinedList.get(i);
-            position = entry[0];
-            int[] direction = entry[1];
-            int newX;
-            int newY;
-            int counter = 0;
-            boolean stopLoop = false;
-            int[] tempPlayerPosition = new int[2];
-
-            while (!stopLoop) {
-                counter++;
-                newX = position[0] + (counter * direction[0]);
-                newY = position[1] + (counter * direction[1]);
-                int[] newPositionArray = new int[] { newX, newY };
-    
-                if (gameBoard.getTilesPerRow() + 1 == newX || gameBoard.getTilesPerRow() + 1 == newY) {
-                    stopLoop = true;
-                }
-    
-                if (isPlayerTile(newPositionArray, player)) {
-                    if (reachedEnemyTile) {
-                        tempPlayerPosition[0] = newX;
-                        tempPlayerPosition[1] = newY;
-                        validDirection.add(new int[] { direction[0], direction[1] });
-                        stopLoop = true;
-                        reachedPlayerTile = true;
-                    }
-                }
-                if (isEnemyTile(newPositionArray, player)){
-                    reachedEnemyTile = true;
-                }
-                if (isTileBlank(newPositionArray)) {
-                    stopLoop = true;
+        for (int i = 0; i < gameBoard.getTilesPerRow(); i++) {
+            for (int j = 0; j < gameBoard.getTilesPerRow(); j++) {
+                if (gameBoard.getButton(i, j) == clickedButton) {
+                    positions[0] = i;
+                    positions[1] = j;
+                    // System.out.println("Button wurde gedrückt --> x:" + i + "|y:" + j);
+                    return positions;
                 }
             }
-    
-    
-            if (!validDirection.isEmpty()) { // Füge diese Bedingung hinzu, um zu überprüfen, ob gültige Richtungsvektoren vorhanden sind
-                for (int j = 1; j < counter; j++) {
-                    for (int[] dir : validDirection) {
-                        newX = position[0] + (j * dir[0]);
-                        newY = position[1] + (j * dir[1]);
-                        changedPositions.add(new int[] { newX, newY });
-                        
-                        if (tempPlayerPosition[0] == newX && tempPlayerPosition[1] == newY) {
-                            break; 
-                        }
-                    }
-                }
-            }
-
         }
-        changedPositions.add(position);
-        if (reachedPlayerTile) {
+
+        return positions;
+    }
+
+    public static void colorize(List < int[] > positionList, Color color) {
+        for (int[] tilePos: positionList) {
+            // System.out.println("Kästschen wurde Eingefärbt --> x:" + tilePos[0] + "|y:" + tilePos[1]);
+            gameBoard.getButton(tilePos[0], tilePos[1]).setBackground(color);;
+        }
+    }
+
+    public static boolean isTileBlank(int[] currentPosition) {
+        // player = 0 (Schwarz) 
+        // player = 1 (Weiß)
+        int x = currentPosition[0];
+        int y = currentPosition[1];
+
+        // Prüft, ob das aktuelle Kästchen leer ist (weder Schwarz noch Weiß).
+        boolean blank = true;
+        // System.out.println("Position erhalten --> x:" + x + "|y:" + y);
+
+        for (int[] tilePos: Main.blackTilesPositionList) {
+            if (tilePos[0] == x && tilePos[1] == y) {
+                blank = false;
+                break;
+            }
+        }
+
+        if (blank) {
+            for (int[] tilePos: Main.whiteTilesPositionList) {
+                if (tilePos[0] == x && tilePos[1] == y) {
+                    blank = false;
+                    break;
+                }
+            }
+        }
+        return blank;
+    }
+
+    private static void addNewPositions(int[] currentPosition, int player) {
+        int[][] vectorsList = { {-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {0, -1}, {0, 1}, {-1, 0}, {1, 0} };
+        List < int[] > validDirectionsList = new ArrayList < > ();
+        List < int[] > validPositionList = new ArrayList < > ();
+        List < int[] > cValidDirectionsList = new ArrayList < > ();
+        List < int[] > cValidPositionList = new ArrayList < > ();
+        boolean isValidTurn = false;
+        for (int[] direction: vectorsList) {
+            int count = 1;
+            int newX, newY;
+            boolean activeLoop = true;
+
+            while (activeLoop) {
+                newX = currentPosition[0] + (count * direction[0]);
+                newY = currentPosition[1] + (count * direction[1]);
+                int[] newPos = new int[] {
+                    newX,
+                    newY
+                };
+
+                if (isEnemyTile(newPos, player) || isPlayerTile(newPos, player)) {
+                    count++;
+                    if (isPlayerTile(newPos, player)) {
+                        validDirectionsList.add(direction);
+                        cValidDirectionsList.add(direction);
+                        validPositionList.add(newPos);
+                        cValidPositionList.add(newPos);
+
+                    }
+                } else {
+                    activeLoop = false;
+                }
+            }
+        }
+        for (int[] vPos: validPositionList) {
+            for (int[] vDir: validDirectionsList) {
+
+                if (currentPosition[0] + vDir[0] == vPos[0] && currentPosition[1] + vDir[1] == vPos[1]) { // gucke ob ein Feld ein gegnerisches Feld zwischen vPos und currentPosition liegt
+                    int[] tempPos = new int[2];
+                    tempPos[0] = currentPosition[0] + vDir[0];
+                    tempPos[1] = currentPosition[1] + vDir[1];
+                    int[] tempDir = new int[2];
+                    tempDir[0] = vDir[0];
+                    tempDir[1] = vDir[1];
+
+                    deleteElementFromArrayList(cValidPositionList, tempPos);
+                    deleteElementFromArrayList(cValidDirectionsList, tempDir);
+                } else {
+
+                }
+            }
+        }
+        List < Integer > validSteps = new ArrayList < > ();
+        for (int[] vPos: cValidPositionList) {
+            int[] directionVector = new int[2];
+            directionVector[0] = vPos[0] - currentPosition[0];
+            directionVector[1] = vPos[1] - currentPosition[1];
+
+            int[] tempPos = new int[2];
+            tempPos[0] = currentPosition[0];
+            tempPos[1] = currentPosition[1];
+
+            int steps = 0;
+
+            while (!Arrays.equals(tempPos, vPos)) {
+                tempPos[0] += Integer.signum(directionVector[0]);
+                tempPos[1] += Integer.signum(directionVector[1]);
+                steps++;
+
+            }
+            validSteps.add(steps);
+        }
+        validPositionList.clear();
+
+        if (!validSteps.isEmpty() && !cValidPositionList.isEmpty() && !cValidDirectionsList.isEmpty() && validPositionList.isEmpty()) {
+            validPositionList.add(currentPosition);
+            isValidTurn = true;
+            for (int i = 0; i < cValidDirectionsList.size(); i++) {
+                int[] directionVector = cValidDirectionsList.get(i);
+                int steps = validSteps.get(i);
+
+                for (int step = 1; step <= steps - 1; step++) {
+                    int newX = currentPosition[0] + (step * directionVector[0]);
+                    int newY = currentPosition[1] + (step * directionVector[1]);
+
+                    // Hinzufügen der Position zu validPositionList
+                    validPositionList.add(new int[] {
+                        newX,
+                        newY
+                    });
+                }
+            }
+        }
+
+        if (isValidTurn) {
             if (player == 0) {
-                for (int[] pos : changedPositions) {
-                    // Entferne die Position aus beiden Listen
+                for (int[] pos: validPositionList) {
                     deleteElementFromArrayList(Main.whiteTilesPositionList, pos);
                     System.out.println("!Position wird Schwarz umgefärbt --> x:" + pos[0] + "|y:" + pos[1]);
-                    // Füge die Position der Schwarzen Liste hinzu
                     Main.blackTilesPositionList.add(pos);
                 }
             } else if (player == 1) {
-                for (int[] pos : changedPositions) {
-                    // Entferne die Position aus beiden Listen
+                for (int[] pos: validPositionList) {
                     deleteElementFromArrayList(Main.blackTilesPositionList, pos);
                     System.out.println("!Position wird Weiß umgefärbt --> x:" + pos[0] + "|y:" + pos[1]);
-                    // Füge die Position der Weißen Liste hinzu
                     Main.whiteTilesPositionList.add(pos);
                 }
+            }
+            Main.nextTurn();
         }
-        Main.nextTurn();
+
     }
-}
-    private static void deleteElementFromArrayList(List<int[]> combinedList, int[] searchedElement) {
+
+    public static boolean isEnemyTile(int[] enemyTile, int player) {
+        // player = 0 (Schwarz) 
+        // player = 1 (Weiß)
+        int x = enemyTile[0];
+        int y = enemyTile[1];
+        List < int[] > playersTilesPositionList = new ArrayList < > (); // Eigene eingefärbte Felder
+        List < int[] > enemysTilesPositionList = new ArrayList < > (); // Gegnerische eingefärbte Felder
+        if (player == 0) {
+            enemysTilesPositionList.addAll(Main.whiteTilesPositionList);
+            playersTilesPositionList.addAll(Main.blackTilesPositionList);
+        } else if (player == 1) {
+            enemysTilesPositionList.addAll(Main.blackTilesPositionList);
+            playersTilesPositionList.addAll(Main.whiteTilesPositionList);
+        }
+
+        for (int[] tile: enemysTilesPositionList) {
+            if (tile[0] == x && tile[1] == y) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    private static boolean isPlayerTile(int[] enemyTile, int player) {
+        // player = 0 (Schwarz) 
+        // player = 1 (Weiß)
+        int x = enemyTile[0];
+        int y = enemyTile[1];
+        List < int[] > playersTilesPositionList = new ArrayList < > (); // Eigene eingefärbte Felder
+        List < int[] > enemysTilesPositionList = new ArrayList < > (); // Gegnerische eingefärbte Felder
+        if (player == 0) {
+            enemysTilesPositionList.addAll(Main.whiteTilesPositionList);
+            playersTilesPositionList.addAll(Main.blackTilesPositionList);
+        } else if (player == 1) {
+            enemysTilesPositionList.addAll(Main.blackTilesPositionList);
+            playersTilesPositionList.addAll(Main.whiteTilesPositionList);
+        }
+
+        for (int[] tile: playersTilesPositionList) {
+            if (tile[0] == x && tile[1] == y) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    private static void deleteElementFromArrayList(List < int[] > combinedList, int[] searchedElement) {
         for (int i = combinedList.size() - 1; i >= 0; i--) {
             int[] element = combinedList.get(i);
             if (Arrays.equals(element, searchedElement)) {
@@ -310,5 +283,5 @@ public class GameAction {
             }
         }
     }
-    
+
 }
